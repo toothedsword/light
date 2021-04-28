0f6425bc30a5bd75b0f8efb8c1ab704febd0aabf
toothedsword <liang_peng_chyg@163.com>
2021-04-28 22:42:12 +0800

    2021年 04月 28日 星期三 22:42:12 CST

--- test4.big.py
+++ test4.big.py
@@ -22,8 +22,8 @@ tctime = tclon*0
 for i in range(0, tclon.size):
     tctime[i] = htt.vec2time(tc[i, 0], tc[i, 1], tc[i, 2], tc[i, 3], 0, 0)
 
-t = htt.str2time('20200830120000', 'yyyymmddHHMMSS')
-t1 = htt.str2time('20200910060000', 'yyyymmddHHMMSS')
+t = np.min(tctime)
+t1 = np.max(tctime)
 
 while t <= t1:
 
