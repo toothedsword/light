aec3cd443c3f754c4ca0d9bd22ce120f0249d7ce
toothedsword <liang_peng_chyg@163.com>
2021-05-25 18:26:30 +0800

    2021年 05月 25日 星期二 18:26:30 CST

---
+++ run_disk.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import re
+import time
+import glob
+import time_htht as htt
+
+
+while True:
+    ct = time.time()
+    year = htt.time2str(ct, 'yyyy')
+    ymd = htt.time2str(ct, 'yyyymmdd')
+    files = glob.glob(
+        './AGRI/L1/FDI/DISK/' + year + '/' + ymd +
+        '/FY4A-_AGRI--_N_DISK_1047E_L1-_FDI-_MULT_NOM_*_4000M_V0001.HDF')
+    for infile in files:
+        rn = '2'
+        outfile = re.sub(r'\/AGRI\/', '/figure/', infile)
+        outfile = re.sub(r'\.HDF$', '.TIFF', outfile)
+        if os.path.exists(outfile):
+            continue
+        print(outfile)
+        outdir = re.sub(r'[^\/]+$', '', outfile)
+        if os.path.exists(outdir):
+            os.system('mkdir -p '+outdir)
+        os.system("python3 ./disk_topo_cth.py "+infile+" "+rn+" "+outfile)
+    time.sleep(1)
