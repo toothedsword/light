e4634def3dff59a88f1642b1fa0467de441adeca
Liang Peng <pengliang@piesat.cn>
2021-09-10 15:29:14 +0800

    2021年 09月 10日 星期五 15:29:14 CST

--- disk_topo_tb.py
+++ disk_topo_tb.py
@@ -27,6 +27,7 @@ def gen_ccc(rgb, ns):
 
 if True:
     rgb = ((0, 1, 0), (0.9, 1, 0.8), (0.5, 0.2, 0))
+    rgb = ((1, 1, 0), (0.9, 1, 0.8))
     ns = [50, 50]
     tcc = gen_ccc(rgb, ns)
 
@@ -147,7 +148,7 @@ tb3 = num2rgb(tb, ch8, rg)
 
 tb_nocloud = tb
 tb_nocloud[np.where(cth <= 0)] = np.NAN
-tb3_nocloud = num2rgb(tb_nocloud, tcc, [10, -20]+273.15)
+tb3_nocloud = num2rgb(tb_nocloud, tcc, [-50, 50]+273.15)
 tb3[np.where(tb3_nocloud > 0)] = tb3_nocloud[np.where(tb3_nocloud > 0)]
 
 lt = light.point(lon_fy4a, lat_fy4a, topo, np.array([-1, 1, 1]))
