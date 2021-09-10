8940c09a72018900563a4a52d457b4dc57d893b7
Liang Peng <pengliang@piesat.cn>
2021-09-10 15:35:55 +0800

    2021年 09月 10日 星期五 15:35:55 CST

--- disk_topo_tb.py
+++ disk_topo_tb.py
@@ -149,7 +149,8 @@ tb3 = num2rgb(tb, ch8, rg)
 tb_nocloud = tb
 tb_nocloud[np.where(cth <= 0)] = np.NAN
 tb3_nocloud = num2rgb(tb_nocloud, tcc, [-50+273.15, 50+273.15])
-tb3[np.where(tb3_nocloud > 0)] = tb3_nocloud[np.where(tb3_nocloud > 0)]
+# tb3[np.where(tb3_nocloud > 0)] = tb3_nocloud[np.where(tb3_nocloud > 0)]
+tb3 = tb3_nocloud
 
 lt = light.point(lon_fy4a, lat_fy4a, topo, np.array([-1, 1, 1]))
 if True:
