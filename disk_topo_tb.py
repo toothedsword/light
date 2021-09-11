01587c7e3f1cee854885ebac3246b808343e5a81
Liang Peng <pengliang@piesat.cn>
2021-09-11 14:15:22 +0800

    2021年 09月 11日 星期六 14:15:22 CST

--- disk_topo_tb.py
+++ disk_topo_tb.py
@@ -148,24 +148,25 @@ rg = [-110+273.15, 50+100+273.15]
 # rg = [-90+273.15, 60+273.15]
 tb3 = num2rgb(tb, ch8, rg)
 
-tb_nocloud = tb
-tb3_nocloud = num2rgb(tb_nocloud, tcc, [-50+273.15, 20+273.15])
-r = tb3[:,:,0]
-g = tb3[:,:,1]
-b = tb3[:,:,2]
-rn = tb3_nocloud[:,:,0]
-gn = tb3_nocloud[:,:,1]
-bn = tb3_nocloud[:,:,2]
-rn[np.where(cth > 0)] = r[np.where(cth > 0)] 
-gn[np.where(cth > 0)] = g[np.where(cth > 0)] 
-bn[np.where(cth > 0)] = b[np.where(cth > 0)] 
+if False:
+    tb_nocloud = tb
+    tb3_nocloud = num2rgb(tb_nocloud, tcc, [-50+273.15, 20+273.15])
+    r = tb3[:,:,0]
+    g = tb3[:,:,1]
+    b = tb3[:,:,2]
+    rn = tb3_nocloud[:,:,0]
+    gn = tb3_nocloud[:,:,1]
+    bn = tb3_nocloud[:,:,2]
+    rn[np.where(cth > 0)] = r[np.where(cth > 0)] 
+    gn[np.where(cth > 0)] = g[np.where(cth > 0)] 
+    bn[np.where(cth > 0)] = b[np.where(cth > 0)] 
 # tb3[np.where(tb3_nocloud > 0)] = tb3_nocloud[np.where(tb3_nocloud > 0)]
-tb3[:,:,0] = rn
-tb3[:,:,1] = gn
-tb3[:,:,2] = bn
-tb3 = tb3_nocloud
-print(tb3.shape)
-print(cth.shape)
+    tb3[:,:,0] = rn
+    tb3[:,:,1] = gn
+    tb3[:,:,2] = bn
+    tb3 = tb3_nocloud
+    print(tb3.shape)
+    print(cth.shape)
 
 lt = light.point(lon_fy4a, lat_fy4a, topo, np.array([-1, 1, 1]))
 if True:
