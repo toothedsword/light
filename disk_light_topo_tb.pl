b07640adb8d2ed420fa3034b4ed96ed52af43c8d
Liang Peng <pengliang@piesat.cn>
2021-09-11 16:44:01 +0800

    2021年 09月 11日 星期六 16:44:01 CST

--- disk_light_topo_tb.pl
+++ disk_light_topo_tb.pl
@@ -1,7 +1,7 @@
 #!/bin/perl
 #
 my $infile = '/mnt/fy4data/AGRI/L1/FDI/DISK/2021/20210910/FY4A-_AGRI--_N_DISK_1047E_L1-_FDI-_MULT_NOM_20210910000000_20210910001459_4000M_V0001.HDF';
-my $res = '4';
+my $res = '2';
 my $outdir = './figure/';
 my $cthfile = '/mnt/fy4data/AGRI/L2/CTH/DISK/NOM/2021/20210910/FY4A-_AGRI--_N_DISK_1047E_L2-_CTH-_MULT_NOM_20210910000000_20210910001459_4000M_V0001.NC';
 my $logfile = './1.log';
