7a067e5e00fb3b4c7d48444fa2bf22a659fb0909
Liang Peng <pengliang@piesat.cn>
2021-09-16 13:30:38 +0800

    2021年 09月 16日 星期四 13:30:38 CST

--- test11.py
+++ test11.py
@@ -15,6 +15,9 @@ import re
 import sys
 import glob
 
+from matplotlib.font_manager import FontProperties
+SIMHEI_TTF = r'/usr/share/fonts/simsun.ttc'
+chn_font = FontProperties(fname='/usr/share/fonts/msyh.ttc')
 
 tcname = sys.argv[1]
 ft = sys.argv[2]
@@ -23,6 +26,8 @@ tcfile = './' + tcname + '.dat'
 tc = np.loadtxt(tcfile)
 tclon = tc[:, 5]
 tclat = tc[:, 4]
+tcmslp = tc[:, 6]
+tcvmax = tc[:, 7]
 tctime = tclon*0
 for i in range(0, tclon.size):
     tctime[i] = htt.vec2time(tc[i, 0], tc[i, 1], tc[i, 2], tc[i, 3], 0, 0)
@@ -37,6 +42,8 @@ while t <= t1:
 
     tclon1 = np.interp(t, tctime, tclon)
     tclat1 = np.interp(t, tctime, tclat)
+    tcmslp1 = np.interp(t, tctime, tcmslp)
+    tcvmax1 = np.interp(t, tctime, tcvmax)
 
     os.system('mkdir -p ./figure/' + ft + '/')
     figname = './figure/' + ft + '/' + tcfile + '_' + ft + '_' +\
@@ -56,7 +63,7 @@ while t <= t1:
     gc.collect()
     fignames = glob.glob(figname+'*.png')
     ext = 0
-    if len(fignames) > 0:
+    if len(fignames) > 0 and False:
         pass
         t = t+60*60
         continue
@@ -74,17 +81,39 @@ while t <= t1:
 
         pn = np.sum(tb2 > 50)/np.sum(lon2 > -360)*100
         spn = str(int(pn))
-        fig = plt.figure(figsize=(8, 6), dpi=400)
+        fig = plt.figure(figsize=(10, 6), dpi=400)
         ax = fig.add_subplot(1, 1, 1)
         f = ax.imshow(
             np.flip(tb4, 0), cmap=ccc,
             extent=(lonlim[0], lonlim[1], latlim[0], latlim[1]))
-        plt.title(htt.time2str(t, 'yyyy/mm/dd HH:MM:SS'))
-        # ax.plot(tclon, tclat, 'r-+', linewidth=0.5)
-        # ax.plot(tclon1, tclat1, 'g+', linewidth=0.5, markersize=20)
+        plt.title(tcname+': ' + htt.time2str(t, 'yyyy/mm/dd HH:MM:SS')+' UTC')
+        # ax.plot(tclon, tclat, 'r-.', linewidth=0.5)
+        ax.plot(tclon1, tclat1, 'k+', linewidth=0.5, markersize=20)
         ax.set_xlim(lonlim)
         ax.set_ylim(latlim)
+        ax.set_position([0.1, 0.1, 0.6, 0.8])
         # ax.plot(cll[:, 0], cll[:, 1], 'k-', linewidth=0.5)
+        # ax.xyouts(lonlim[0], latlim[0], 'xxxx')
+
+        dx = (lonlim[1]-lonlim[0])/5
+        dy = (latlim[1]-latlim[0])/10
+        x0 = lonlim[1]+dx
+        y0 = latlim[1]-dy
+        ll = str(tclon1)+'E, '+str(tclat1)+'N'
+        ll = re.sub(r'(\.\d\d)\d+', r'\1', ll)
+
+        tlist = ['台风位置和强度信息：', 
+                '台风中心位置：', ll, 
+                '最大持续风速：'+str(int(tcvmax1*0.51))+'m/s', 
+                '中心气压：'+str(tcmslp1)+'hPa']
+
+        itlist = -1
+        for tlisti in tlist:
+            y0 = y0 - dy
+            ax.text(x0, y0, tlisti, 
+                    fontproperties=chn_font, 
+                    fontsize=14, verticalalignment="center", 
+                    horizontalalignment="left")
 
         ax, _ = mpl.colorbar.make_axes(plt.gca(), shrink=0.5)
         cbar = mpl.colorbar.ColorbarBase(
